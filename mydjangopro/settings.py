04afead7c6393bd23fbd49a5bdb8320b0fc8eda0
monica16031998 <monicaamsalas@gmail.com>
2025-08-04 08:44:47 +0530

    Update settings.py

--- mydjangopro/settings.py
+++ mydjangopro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-26gv+a=!&h3fgeut*j^1x^cbivtjl#3japh51v4_f4zbnkd(_b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
